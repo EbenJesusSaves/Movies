4c93025632ede728379e1675d67b63a230402b03
Ebenezer <agyekumebenezer2001@gmail.com>
2022-11-28 11:50:05 +0000

    Mega Movies

--- App.js
+++ App.js
@@ -1,6 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+import {
+  useFonts as useLexnd,
+  Lexend_400Regular,
+  Lexend_500Medium,
+  Lexend_700Bold,
+} from "@expo-google-fonts/lexend";
+
+import {
+  useFonts as useAlata,
+  alata_400Regular,
+} from "@expo-google-fonts/alata";
+
+//this loads the lexend fonts
+const [LextedLoad] = useLexnd({
+  Lexend_400Regular,
+});
+
+//this loads the alata fonts
+
+const [AlataLoaded] = useAlata({
+  alata_400Regular,
+});
+
+//checks before loading the app
+if (!AlataLoaded || !LextedLoad) {
+  return null;
+}
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
